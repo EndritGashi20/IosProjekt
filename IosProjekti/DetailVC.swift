744e959f751e2b03992aa3483690a1b7ab3be34c
Endrit <endritg60@gmail.com>
2023-08-19 03:59:14 -0700

    updating detail controler

--- IosProjekti/DetailVC.swift
+++ IosProjekti/DetailVC.swift
@@ -7,38 +7,50 @@
 
 import UIKit
 
-class DetailVC: UIViewController {
+class DetailVC: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
     @IBOutlet weak var dTitle: UITextField!
-       @IBOutlet weak var dShortDesc: UITextField!
-       @IBOutlet weak var selectImage: UIImageView!
-       
-       override func viewDidLoad() {
-           super.viewDidLoad()
-
-           // Do any additional setup after loading the view.
-       }
-    @IBAction func btnSelectImage(_ sender: Any) {
-            let picker = UIImagePickerController()
-            picker.delegate = self
-            picker.allowsEditing = true
-            picker.sourceType = .photoLibrary
-            present(picker, animated: true)
-            
-        }
-    @IBAction func btnSave(_ sender: Any) {
-           
-           let title = dTitle.text ?? ""
-           let shortDesc = dShortDesc.text ?? ""
-           
-           if let imageData = selectImage.image?.jpegData(compressionQuality: 1) {
-               DatabaseHelper.shareInstance.save(title: title, shortdescription: shortDesc, image: imageData)
-           }
-           
-           _ = navigationController?.popViewController(animated: true)
-           
-       }
+    @IBOutlet weak var dShortDesc: UITextField!
+    @IBOutlet weak var selectImage: UIImageView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnSelectImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+        
+    }
+    
+    @IBAction func btnSave(_ sender: Any) {
+        
+        let title = dTitle.text ?? ""
+        let shortDesc = dShortDesc.text ?? ""
+        
+        if let imageData = selectImage.image?.jpegData(compressionQuality: 1) {
+            DatabaseHelper.shareInstance.save(title: title, shortdescription: shortDesc, image: imageData)
+        }
+        
+        _ = navigationController?.popViewController(animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let userPickedImage = info[.editedImage] as? UIImage else { return }
+        
+        selectImage.image = userPickedImage
+        
+        picker.dismiss(animated: true)
+        
+    }
+    
     /*
     // MARK: - Navigation
 
